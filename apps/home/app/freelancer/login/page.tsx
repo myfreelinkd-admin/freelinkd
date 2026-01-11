cf5c58cf61b15f00d58425e82b26d4f2544e034a
Alvin Putra <vynuta12@gmail.com>
2026-01-11 20:47:25 +0700

    panggilan

--- apps/home/app/freelancer/login/page.tsx
+++ apps/home/app/freelancer/login/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from "react";
 import FreelancerLogin from "@/app/components/login/freelancer/freelancer-login";
 
 export default function FreelancerLoginPage() {
-  return <FreelancerLogin />;
+  return (
+    <Suspense>
+      <FreelancerLogin />
+    </Suspense>
+  );
 }
